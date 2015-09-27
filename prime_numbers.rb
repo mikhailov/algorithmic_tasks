44bcda7966b56244a1c4b9093d470f8b402a14c2
mikhailov <mikhailov.anatoly@gmail.com>
2015-09-27 17:57:50 +0100

    prime numbers - speed up 20% via stop loop after third of size, more tests on big data set

--- prime_numbers.rb
+++ prime_numbers.rb
@@ -15,12 +15,14 @@ class PrimeNumbers
     @prime
   end
 
+
+
   private
 
   def inner_loop(e, is_prime = true)
-    half = e / 2
+    third = e / 3
     3.step(e-1, 2) do |k|
-      break if k > half
+      break if k > third
       if e % k == 0
         is_prime = false
         break
@@ -52,7 +54,11 @@ class PrimeNumbersTest < Minitest::Unit::TestCase
     assert_equal [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97], PrimeNumbers.new(100).process
   end
 
-  def test_invalid_5
+  def test_valid_5
+    assert_equal [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797], PrimeNumbers.new(800).process
+  end
+
+  def test_invalid_6
     assert_raises( ArgumentError ) { PrimeNumbers.new(1) }
   end
 
